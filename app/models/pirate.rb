bde0b6af301d3522f246ebad64baae7478c960ba
Daniel J Feleppa <dfeleppa@gmail.com>
2020-05-20 12:53:40 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,10 +3,10 @@ attr_accessor :name, :weight, :height
 
   @@all = []
  
-  def initialize(params)
-    @name = params[:name]
-    @weight= params[:weight]
-    @height = params[:height]
+  def initialize(args)
+    @name = args[:name]
+    @weight= args[:weight]
+    @height = args[:height]
     @@all << self
   end
  
