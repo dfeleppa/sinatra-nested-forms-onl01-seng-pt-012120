be2e57c95d8a6af7a07bfb6cf57b15388dcaeadf
Daniel J Feleppa <dfeleppa@gmail.com>
2020-05-20 12:49:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,20 +18,8 @@ module FormsLab
         Ship.new(details)  
       end
       @ships = Ships.all
-      
       erb :"pirates/show"
     end
     
-    post '/pirates' do
-
-      @pirate = Pirate.new(params[:pirate])
-
-      params[:pirate][:ships].each do |details|
-        Ship.new(details)
-      end
-      @ships = Ship.all
-      erb :'pirates/show'
-    end
-    
-  end
+   end
 end
